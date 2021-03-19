3c7c2183e7cf1307310d9f9326d3d7757a37dea6
toffee05 <allenabit05@gmail.com>
2021-03-19 08:42:50 +0800

    Game Finished

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,6 @@
 let interval, lvl = 3, xTemp = 0, yTemp = 0, colorTemp = "white", i = 0, i2 = 0, i3 = 0, score = 0, locatorTemp = [-1, -1, "temp"], sq = 0;
-badTile = 0, newLvl = false, rotate = 0, click = new Audio("../assets/audio/click.mp3"), flag = false,
+badTile = 0, rotate = 0, click = new Audio("../assets/audio/click.mp3"),
 locator = [...Array(lvl)].map(e => Array(lvl));
-
-const sleep = (delay) => new Promise((done) => setTimeout(done, delay));
 
 $(".home").click(function() {
 	location.href = "../index.html";
@@ -15,7 +13,7 @@ $(".start").click(function() {
 
 function start() {
 	$("#lvl span").html(lvl - 2);
-	if(sq % 2 == 1 || newLvl == true) {
+	if(sq % 2 == 1) {
 		$("img").attr("src", "../assets/images/badTile.png");
 		$(".start").html("Quit");
 	}
@@ -25,45 +23,41 @@ function start() {
 	}
 	randomize();
 	i2 = 0;
-	interval = setInterval(draw, 1000 / 3 - 50);
+	interval = setInterval(draw, 1000 / 3 + 10);
 	i3 = 0;
-	setTimeout(user, 1000 / 3 * lvl + 10);
+	setTimeout(user, 1000 / 3 * lvl + 20);
 }
 			
 function randomize() {
 	(function () { locator = [...Array(lvl)].map(e => Array(lvl)); i = 0; badTile = 0; }());
 	for(i = 0; i < lvl;) {
-		xTemp = Math.floor(Math.random() * 5);
-		yTemp = Math.floor(Math.random() * 5);
-		colorTemp = Math.floor(Math.random() * 5);
-		if(typeof(locator[i][0]) === 'undefined' && (locatorTemp[0] !== xTemp + 1 && locatorTemp[1] !== yTemp + 1)) {
+		xTemp = Math.floor(Math.random() * 5) + 1;
+		yTemp = Math.floor(Math.random() * 5) + 1;
+		colorTemp = Math.floor(Math.random() * 4);
+		if(typeof(locator[i][0]) === 'undefined' && (locatorTemp[0] !== xTemp && locatorTemp[1] !== yTemp)) {
 			if(colorTemp > 2 && badTile < lvl - 1) {
-				locator[i] = [(xTemp + 1), (yTemp + 1), "../assets/images/badTile.png"];
+				locator[i] = [xTemp, yTemp, "../assets/images/badTile.png"];
 				badTile++;
 			}
 			else
-				locator[i] = [(xTemp + 1), (yTemp + 1), "../assets/images/goodTile.png"];
+				locator[i] = [xTemp, yTemp, "../assets/images/goodTile.png"];
 			locatorTemp = locator[i];
 			i++;
 		}
 	}
+	(function () { i = 0; }());
 }
 			
 function draw() {
-	if(sq % 2 == 0)
-		return;
 	$(".x" + locator[i2][0] + ".y" + locator[i2][1] + " .cellInner .cellBack img").attr("src", locator[i2][2]);
 	$(".x" + locator[i2][0] + ".y" + locator[i2][1] + " .cellInner").addClass("flipActivate");
 	setTimeout(function() {
 		$(".x" + locator[i2][0] + ".y" + locator[i2][1] + " .cellInner").removeClass("flipActivate");
-	}, 200);
-	(async function() {
-		await sleep(200);
 		i2++;
-	}());
+	}, 200);
+	
 	if(i2 == lvl - 1) {
 		clearInterval(interval);
-		(function () { i = 0; }());
 	}
 }
 			
@@ -83,12 +77,10 @@ function user() {
 		if(input[0] != locator[i][0] || input[1] != locator[i][1]) {
 			sq = 0;
 			newLvl = false;
-			alert("You Lost");
-			lose();
+			modal(false);
 		}
 		else if(i3 + 1 == lvl - badTile) {
-			alert("Nice! Next Level.");
-			win();
+			modal(true);
 		}
 		else
 			i3++;
@@ -97,21 +89,61 @@ function user() {
 }
 			
 function win() {
+	$(".modal").css("display", "none");
+	$(".modal-content").removeClass("drop");
+	$(".proceed").off("click");
+	$(".quit").off("click");
+	$(".close").off("click");
 	i = 0, i2 = 0, i3 = 0;
 	$(".cellInner").removeClass("flipActivate");
 	lvl++;
-	newLvl = true;
-	start(sq, true);
+	start();
 }
 			
 function lose() {
+	$(".modal").css("display", "none");
+	$(".modal-content").removeClass("drop");
+	$(".proceed").off("click");
+	$(".quit").off("click");
+	$(".close").off("click");
 	i = 0, i2 = 0, i3 = 0;
 	$("img").removeAttr("src");
 	$(".cellInner").removeClass("flipActivate");
 	$(".start").html("Start");;
-	alert("You reached lvl " + (lvl - 2));
 	$("#lvl span").html(1);
 	$("#points span").html(0);
-	lvl = 3, score = 0;
+	lvl = 3, score = 0, sq = 0;
 	$(".cell").off();
+}
+
+function modal(wl) {
+	if(wl)
+		$(".modal-content p").html("You Have Safely Ascended a Level. Proceed?");
+	else
+		$(".modal-content p").html("You Have Died.");
+	
+	$(".modal").css("display", "flex");
+	$(".modal-content").addClass("drop");
+	
+	if(wl) {
+		$(".close").css("display", "none");
+		$(".continue").css("display", "block");
+		
+		$(".proceed").click(function() {
+			win();
+		});
+
+		$(".quit").click(function() {
+			modal(false);
+		});
+	}
+	
+	else {
+		$(".close").css("display", "flex");
+		$(".continue").css("display", "none");
+		
+		$(".close").click(function() {
+			lose();
+		});
+	}
 }
